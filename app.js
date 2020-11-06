e825d7d5b614748f96f589ba586a039048dfae34
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2020-11-06 12:56:22 -0500

    add example code

--- app.js
+++ app.js
@@ -6,9 +6,29 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.end("Hello Worlddd");
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+const parse = (url) => {
+
+}
+
+/* Example express app below
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+ */
