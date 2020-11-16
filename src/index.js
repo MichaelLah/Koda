adb92e0fb524d96b1ee602bb32af81cae022f3fc
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2020-11-15 19:12:29 -0500

    basic routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-var http = require("http");
-var Router = require("./router");
+const http = require("http");
+const Router = require("./router");
+const router = new Router();
+const fs = require("fs").promises;
 
-var router = new Router();
-console.log(router);
+router.get("/some/path", (req, res) => {
+  console.log("some path!");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("./src/views/some_page.html", null).then((data) => {
+    res.write(data);
+    res.end();
+  });
+});
+
+router.get("/", (req, res) => {
+  console.log("base path");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("./src/views/base_page.html", null).then((data) => {
+    res.write(data);
+    res.end();
+  });
+});
 
 const server = http.createServer(function (req, res) {
   router.eval(req, res);
-  res.end();
+  // res.end();
 });
 
 server.listen(8080);
--- src/router.js
+++ src/router.js
@@ -1,22 +1,55 @@
+const fs = require("fs").promises;
+// const fs = require("fs");
+
 class Router {
   // handers = [];
 
+  constructor() {
+    this.routes = {};
+  }
+
+  get(path, handler) {
+    this._set_route(`GET:${path}`, handler);
+  }
+  post(path, handler) {
+    this._set_route(`POST:${path}`, handler);
+  }
+  put(path, handler) {
+    this._set_route(`PUT:${path}`, handler);
+  }
+  delete(path, handler) {
+    this._set_route(`DELETE:${path}`, handler);
+  }
+
   eval(req, res) {
-    const x = {
-      "GET:/": function (req, res) {
-        res.write(`${req.method}:${req.url}`);
-      },
-    };
+    const { url, method } = req;
+    if (!this.routes[`${method}:${url}`]) {
+      this.notFound(req, res);
+      return;
+    }
+    this.routes[`${method}:${url}`](req, res);
 
-    x[`${req.method}:${req.url}`];
-
-    x[`${req.method}:${req.url}`](req, res);
+    // x[`${req.method}:${req.url}`];
+    //
+    // x[`${req.method}:${req.url}`](req, res);
 
     // if (req.method == "GET" && req.url == "/") {
     //   res.write("GET /");
     // } else {
     //   res.write("welcome to kado hello!");
     // }
+  }
+
+  _set_route(path, handler) {
+    this.routes[path] = handler;
+  }
+
+  notFound(req, res) {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    fs.readFile("./src/404.html", null).then((data) => {
+      res.write(data);
+      res.end();
+    });
   }
 }
 
