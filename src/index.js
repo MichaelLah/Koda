fe0d16d5c741a2e80b06bf93e946e0f966b4b9c2
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2020-11-20 13:29:33 -0500

    updates from today

--- src/index.js
+++ src/index.js
@@ -3,36 +3,48 @@ const Router = require("./router");
 const router = new Router();
 const fs = require("fs").promises;
 
-router.get("/some/path", (req, res) => {
+router.get("/some2/:name", (ctx) => {
   console.log("some path!");
-  res.writeHead(200, { "Content-Type": "text/html" });
+  ctx.res.writeHead(200, { "Content-Type": "text/plain" });
+  ctx.res.write(`${ctx.params.name}`);
+  ctx.res.end();
+});
+
+// some/123/path/456
+// some/antother_path/path/456
+router.get("/some/:name", (ctx) => {
+  console.log("some path!");
+  ctx.res.writeHead(200, { "Content-Type": "text/html" });
   fs.readFile("./src/views/some_page.html", null).then((data) => {
-    res.write(data);
-    res.end();
+    ctx.es.write(data);
+    ctx.res.end();
   });
 });
 
-router.get("/", (req, res) => {
+router.get("/some/:name", (ctx) => {
+  console.log("some path!");
+  ctx.res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("./src/views/some_page.html", null).then((data) => {
+    ctx.res.write(data);
+    ctx.res.end();
+  });
+});
+
+router.get("/", (ctx) => {
   console.log("base path");
-  res.writeHead(200, { "Content-Type": "text/html" });
+  ctx.res.writeHead(200, { "Content-Type": "text/html" });
   fs.readFile("./src/views/base_page.html", null).then((data) => {
-    res.write(data);
-    res.end();
+    ctx.res.write(data);
+    ctx.res.end();
   });
 });
 
 const server = http.createServer(function (req, res) {
-  router.eval(req, res);
-  // res.end();
+  const ctx = {
+    req,
+    res
+  };
+  router.eval(ctx);
 });
 
 server.listen(8080);
-
-/*
-router
-  .get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  .get('/hello', (req, res) => {
-  })
-*/
--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 const fs = require("fs").promises;
+const { pathToRegexp, match, parse, compile, exec } = require("path-to-regexp");
 // const fs = require("fs");
 
 class Router {
@@ -6,49 +7,61 @@ class Router {
 
   constructor() {
     this.routes = {};
+    this.routes["GET"] = new Map();
+    this.routes["POST"] = new Map();
+    this.routes["PUT"] = new Map();
+    this.routes["DELETE"] = new Map();
   }
 
   get(path, handler) {
-    this._set_route(`GET:${path}`, handler);
+    this._set_route("GET", path, handler);
   }
   post(path, handler) {
-    this._set_route(`POST:${path}`, handler);
+    this._set_route("POST", path, handler);
   }
   put(path, handler) {
-    this._set_route(`PUT:${path}`, handler);
+    this._set_route("PUT", path, handler);
   }
   delete(path, handler) {
-    this._set_route(`DELETE:${path}`, handler);
+    this._set_route("DELETE", path, handler);
   }
 
-  eval(req, res) {
-    const { url, method } = req;
-    if (!this.routes[`${method}:${url}`]) {
-      this.notFound(req, res);
+  eval(ctx) {
+    const { url, method } = ctx.req;
+    if (!this.routes[method]) {
+      this.notFound(ctx);
       return;
     }
-    this.routes[`${method}:${url}`](req, res);
-
-    // x[`${req.method}:${req.url}`];
-    //
-    // x[`${req.method}:${req.url}`](req, res);
-
-    // if (req.method == "GET" && req.url == "/") {
-    //   res.write("GET /");
-    // } else {
-    //   res.write("welcome to kado hello!");
-    // }
+    // if (!this.routes[method])
+    for (let key of this.routes[method].keys()) {
+      // for (let key in this.routes[method]) {
+      console.log("eval");
+      console.log(typeof key);
+      const matched = key(url);
+      if (matched) {
+        ctx.params = matched.params;
+        this.routes[method].get(key)(ctx);
+      }
+    }
   }
 
-  _set_route(path, handler) {
-    this.routes[path] = handler;
+  _set_route(method, path, handler) {
+    // const route = pathToRegexp(path);
+    const route = match(path, { deocde: decodeURIComponent });
+    console.log(route);
+    // console.log(typeof route);
+    // console.log(route.exec("/"));
+    // d = {};
+    // d[route] = "one";
+    // d;
+    this.routes[method].set(route, handler);
   }
 
-  notFound(req, res) {
-    res.writeHead(404, { "Content-Type": "text/html" });
+  notFound(ctx) {
+    ctx.res.writeHead(404, { "Content-Type": "text/html" });
     fs.readFile("./src/404.html", null).then((data) => {
-      res.write(data);
-      res.end();
+      ctx.res.write(data);
+      ctx.res.end();
     });
   }
 }
